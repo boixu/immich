c7c4436e0ab7001c531cc42889a802a14fd2cdab
Dan <admin@boixu.com>
2024-07-05 14:55:59 +0300

    test proxy

---
+++ functions/_middleware.ts
@@ -0,0 +1,33 @@
+import proxyflare from "@flaregun-net/proxyflare-for-pages"
+
+const routes: Route[] = []
+
+// `PagesFunction` is from @cloudflare/workers-types
+export const onRequest: PagesFunction[] = [
+  (context) =>
+    proxyflare({
+      config: {
+        global: { debug: true },
+        routes,
+      },
+    })(context),
+  // other Pages plugins and middleware
+]
+
+const routes: Route[] = [
+  {
+    from: {
+      pattern: "photos.home31.pw/api",
+      alsoMatchWWWSubdomain: true,
+    },
+    to: { url: "https://lumin.home31.pw/api" },
+  },
+  {
+    from: {
+      pattern: "photos.home31.pw/.well-known/immich",
+      alsoMatchWWWSubdomain: true,
+    },
+    to: { url: "https://lumin.home31.pw/.well-known/immich" },
+  },
+
+]
