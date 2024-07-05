ee28c75003c6c1c1c1fda98d088831768f9c120f
Dan <admin@boixu.com>
2024-07-05 18:00:39 +0300

    test proxy

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -13,29 +13,14 @@ export const onRequest: PagesFunction[] = [
 ]
 
 const routes: Route[] = [
-  {
-    from: {
-      pattern: "photos.home31.pw/api/*",
-    },
-    to: {
-      url: "lumin.home31.pw/api"
-      website: {
-        mode: "true",
-        resources: [],
-      },
+ {
+   from: { pattern: "photos.home31.pw/api/*" },
+   to: {
+     url: "lumin.home31.pw/api",
+     website: {
+       mode: true,
+       resources: [],
      },
-    },
-  },
-  {
-    from: {
-      pattern: "photos.home31.pw/.well-known/immich",
-    },
-    to: { url: "https://lumin.home31.pw/.well-known/immich" },
-  },
-  {
-    from: {
-      pattern: "photos.home31.pw/custom.css",
-    },
-    to: { url: "https://lumin.home31.pw/custom.css" },
-  },
+   },
+ }
 ]
